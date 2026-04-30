83c9c38e580fbbf3bbbef52792b7ef518b0f979d
2400031466Lasya <lasya.sucharitha15@gmail.com>
2026-05-01 00:56:29 +0530

    done

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://fsad-scholarship-project-backend-12.onrender.com/api",
 });
 
 // Attach token automatically
